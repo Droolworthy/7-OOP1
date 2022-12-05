a68b1587899894e290f5eb72cc2ffdc02992b1ce
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-12-05 18:28:28 +0300

    Добавьте файлы проекта.

---
+++ 7-OOP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _7_OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new(100,100,25);
+
+            player.ShowStats();
+        }
+    }
+
+    public class Player 
+    {
+        public int Health;
+        public int Stamina;
+        public int Attack;
+
+        public Player(int health, int stamina, int attack)
+        {
+            Health = health;
+            Stamina = stamina;
+            Attack = attack;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine("Здоровье - " + Health + "\nВыносливость - " + Stamina + "\nАтака - " + Attack);
+        }
+    }
+}
